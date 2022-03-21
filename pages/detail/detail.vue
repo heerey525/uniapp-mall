a9e1db14543433b88636721dd7e352c86edbb786
heerey525 <190182347@qq.com>
2022-03-21 23:42:21 +0800

    text组件的基本使用

--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -1,5 +1,24 @@
 <template>
-	<view>详情页</view>
+	<view>
+		<view>
+			<text>普通text文本</text>
+		</view>
+		<view>
+			<text selectable>长按可选中</text>
+		</view>
+		<view>
+			<text selectable space="emsp">中文字符空格 一半大小</text>
+		</view>
+		<view>
+			<text selectable space="emsp">中文字符空格 大小</text>
+		</view>
+		<view>
+			<text selectable space="nbsp" style="font-size: 20px;">根据字体设置的空格 大小</text>
+		</view>
+		<view>
+			<text>decode可解码&amp;</text>
+		</view>
+	</view>
 </template>
 
 <script>

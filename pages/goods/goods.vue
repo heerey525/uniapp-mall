b3d45a3e5a1ece96f63cb766cb020c63b9212a96
heerey525 <190182347@qq.com>
2022-03-24 21:56:54 +0800

    完成上拉刷新，下拉加载

--- pages/goods/goods.vue
+++ pages/goods/goods.vue
@@ -1,6 +1,7 @@
 <template>
 	<view class="goodsBox">
 		<goods-list :goods="goods"></goods-list>
+		<view class="isOver" v-if="visible">------我是有底线的------</view>
 	</view>
 </template>
 
@@ -10,24 +11,53 @@
 		name: 'goods',
 		components: { GoodsList },
 		data() {
-			return { goods: [], pageindex: 1, }
+			return {
+				goods: [],
+				pageindex: 1,
+				visible: false,
+			}
 		},
 		onLoad() {
 			this.getHotGoods()
 		},
+		// 下拉加载
+		onReachBottom() {
+			if (this.visible) return
+			this.pageindex++
+			this.getHotGoods()
+		},
+		// 上拉刷新
+		onPullDownRefresh() {
+			this.goods = []
+			this.pageindex = 1
+			this.visible = false
+			this.getHotGoods('isPullDown')
+		},
 		methods: {
 			// 获取推荐商品
-			async getHotGoods() {
+			async getHotGoods(isPullDown) {
 				const res = await this.$myRequest({ url: '/api/getgoods?pageindex=' + this.pageindex })
-				console.log('获取推荐商品', res)
-				this.goods = res.data.message
+				if (isPullDown) { // 只有上拉刷新才执行
+					setTimeout(() => {
+						uni.stopPullDownRefresh()
+					}, 1000)
+				}
+				this.goods = [...this.goods, ...res.data.message]
+				if (res.data.message.length < 10) this.visible = true
 			}
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
 	.goodsBox {
 		background-color: #EEEEEE;
+		.isOver {
+			width: 100%;
+			height: 100rpx;
+			line-height: 100rpx;
+			text-align: center;
+			color: #CCCCCC;
+		}
 	}
 </style>

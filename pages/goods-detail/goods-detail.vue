e5d7af6519026c839b0420d3cc3c59a8ab72e999
heerey525 <190182347@qq.com>
2022-03-25 22:55:17 +0800

    完成商品详情

---
+++ pages/goods-detail/goods-detail.vue
@@ -0,0 +1,186 @@
+<template>
+	<view class="goods_detail">
+		<!-- 轮播图区域 -->
+		<swiper class="swiper" indicator-dots circular>
+			<swiper-item v-for="item in swipers">
+				<image :src="item.src"></image>
+			</swiper-item>
+		</swiper>
+		<view class="header">
+			<view class="price">
+				<text>￥{{ goodsInfo.sell_price }}</text>
+				<text>￥{{ goodsInfo.market_price }}</text>
+			</view>
+			<view class="goods_name">{{ goodsInfo.title }}</view>
+		</view>
+		<view class="line"></view>
+		<view class="container">
+			<view>货号：{{ goodsInfo.goods_no }}</view>
+			<view>库存：{{ goodsInfo.stock_quantity }}</view>
+		</view>
+		<view class="line"></view>
+		<view class="footer">
+			<view class="title">
+				详情介绍
+			</view>
+			<view class="content">
+				<rich-text :nodes="content"></rich-text>
+			</view>
+		</view>
+		<view class="goods_nav">
+			<uni-goods-nav :fill="true" :options="options" :buttonGroup="buttonGroup" @click="onClick"
+				@buttonClick="buttonClick" />
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				id: '',
+				swipers: [],
+				goodsInfo: {},
+				content: '',
+				options: [{
+					icon: 'headphones',
+					text: '客服'
+				}, {
+					icon: 'shop',
+					text: '店铺',
+					info: 2,
+					infoBackgroundColor: '#007aff',
+					infoColor: "red"
+				}, {
+					icon: 'cart',
+					text: '购物车',
+					info: 2
+				}, ],
+				buttonGroup: [{
+						text: '加入购物车',
+						backgroundColor: '#ff0000',
+						color: '#fff'
+					},
+					{
+						text: '立即购买',
+						backgroundColor: '#ffa200',
+						color: '#fff'
+					}
+				]
+			}
+		},
+		onLoad(options) {
+			this.id = options.id
+			console.log('this.id', this.id)
+			this.getSwipers()
+			this.getInfos()
+			this.getDesc()
+		},
+		methods: {
+			// 获取详情轮播图
+			async getSwipers() {
+				const res = await this.$myRequest({ url: '/api/getthumimages/' + this.id })
+				console.log('获取详情轮播图', res)
+				this.swipers = res.data.message
+			},
+			// 获取详情参数
+			async getInfos() {
+				const res = await this.$myRequest({ url: '/api/goods/getinfo/' + this.id })
+				console.log('获取详情参数', res)
+				this.goodsInfo = res.data.message[0]
+			},
+			// 获取详细介绍
+			async getDesc() {
+				const res = await this.$myRequest({ url: '/api/goods/getdesc/' + this.id })
+				console.log('获取详细介绍', res)
+				this.content = res.data.message[0].content
+			},
+			onClick (e) {
+				uni.showToast({
+					title: `点击${e.content.text}`,
+					icon: 'none'
+				})
+			},
+			buttonClick (e) {
+				console.log(e)
+				this.options[2].info++
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.goods_detail {
+		.swiper {
+			width: 750rpx;
+			height: 700rpx;
+
+			image {
+				width: 100%;
+				height: 100%;
+			}
+		}
+
+		.header {
+			padding: 20rpx;
+
+			.price {
+				font-size: 35rpx;
+				color: $mall-color-primary;
+				line-height: 80rpx;
+
+				text:nth-child(2) {
+					color: #CCCCCC;
+					font-size: 25rpx;
+					text-decoration: line-through;
+					margin-left: 20rpx;
+				}
+			}
+
+			.goods_name {
+				font-size: 32rpx;
+				line-height: 60rpx;
+			}
+		}
+
+		.line {
+			height: 10rpx;
+			width: 100%;
+			background-color: #EEEEEE;
+		}
+
+		.container {
+			padding: 10rpx;
+			font-size: 32rpx;
+			line-height: 70rpx;
+		}
+
+		.footer {
+			padding-bottom: 100rpx;
+
+			.title {
+				font-size: 32rpx;
+				padding-left: 10rpx;
+				border-bottom: 2rpx solid #EEEEEE;
+				line-height: 70rpx;
+			}
+
+			.content {
+				padding: 10rpx;
+				font-size: 30rpx;
+				line-height: 50rpx;
+
+				/deep/.gomeImgLoad {
+					width: 730rpx;
+					height: auto;
+				}
+			}
+		}
+		
+		.goods_nav {
+			position: fixed;
+			bottom: 0;
+			width: 100%;
+		}
+	}
+</style>

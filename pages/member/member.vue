c259f197b0bcdfb0be3df475da4ddb5d73e2c953
heerey525 <190182347@qq.com>
2022-03-26 23:10:43 +0800

    完成用户登录

--- pages/member/member.vue
+++ pages/member/member.vue
@@ -1,6 +1,13 @@
 <template>
-	<view>
-		会员
+	<view class="member">
+		<view class="userInfo" v-if="userInfo.nickName">
+			<image :src="userInfo.avatarUrl"></image>
+			<text>{{ userInfo.nickName }}</text>
+		</view>
+		<view class="userInfo" @click="login" v-else>
+			<image src="../../static/icon/member.png"></image>
+			<text>未登录</text>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +15,54 @@
 	export default {
 		data() {
 			return {
-				
+				userInfo: {}
 			}
 		},
+		onShow() {
+			const userInfo = uni.getStorageSync('userInfo')
+			this.userInfo = userInfo
+		},
 		methods: {
-			
+			login() {
+				// #ifndef H5 || MP-WEIXIN
+				uni.getUserInfo({
+					success(res) {
+						console.log(res)
+					}
+				})
+				// #endif
+				// #ifdef MP-WEIXIN
+				uni.getUserProfile({
+					desc: '展示',
+					success(res) {
+						uni.setStorageSync('userInfo', res.userInfo)
+					}
+				})
+				// #endif
+			}
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+	.member {
+		.userInfo {
+			height: 100rpx;
+			// background-color: red;
+			padding: 20rpx 40rpx;
+			line-height: 100rpx;
+			display: flex;
+			border-bottom: 2rpx solid #CCCCCC;
+			
+			
+			image {
+				height: 100rpx;
+				width: 100rpx;
+				margin-right: 60rpx;
+				display: block;
+				border-radius: 50%;
+				border: 2rpx solid #CCCCCC;
+			}
+		}
+	}
 </style>
